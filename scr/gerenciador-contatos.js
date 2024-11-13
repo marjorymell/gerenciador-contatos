bce962798c5c4a8f546d7c6d162cc741a76c19da
marjorymell <marjorymel48@gmail.com>
2024-11-12 21:35:40 -0300

    feat: implement the main logic of the contact manager

---
+++ scr/gerenciador-contatos.js
@@ -0,0 +1,31 @@
+import { GrupoContatos, ContatoIndividual } from "./models.js";
+import { BuscaPorNome } from "./strategies.js";
+
+export class GerenciadorContatos {
+  constructor() {
+    this.raiz = new GrupoContatos();
+    this.estrategiaBusca = new BuscaPorNome();
+  }
+
+  setEstrategiaBusca(estrategia) {
+    this.estrategiaBusca = estrategia;
+  }
+
+  adicionar(contato) {
+    this.raiz.adicionar(new ContatoIndividual(contato));
+  }
+
+  remover(contato) {
+    this.raiz.remover(contato);
+  }
+
+  listar() {
+    return this.raiz.listar();
+  }
+
+  buscar(termo) {
+    return this.raiz.buscar((contato) =>
+      this.estrategiaBusca.execute(contato, termo)
+    );
+  }
+}
